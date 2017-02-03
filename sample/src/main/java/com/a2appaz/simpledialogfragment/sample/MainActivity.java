3b4abd74cc8aafacc80a09faff670f32779f97cf
jarzul <jarzul@2appaz.com>
2017-02-03 11:31:15 +1100

    Feat:(Main Content) Added examples of custom positive and custom negative buttons, with listeners Feat:(Main Content) Added example of a non-cancelable dialog

--- sample/src/main/java/com/a2appaz/simpledialogfragment/sample/MainActivity.java
+++ sample/src/main/java/com/a2appaz/simpledialogfragment/sample/MainActivity.java
@@ -14,7 +14,9 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements SimpleDialogFragmentListener {
 
-    private static final int SIMPLE_DIALOG_REQUEST_CODE = 1;
+    private static final int SIMPLE_DIALOG_ONE_BUTTON_REQUEST_CODE = 1;
+    private static final int SIMPLE_DIALOG_TWO_BUTTONS_REQUEST_CODE = 2;
+    private static final int SIMPLE_DIALOG_NOT_CANCELABLE_REQUEST_CODE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +40,59 @@ public class MainActivity extends AppCompatActivity implements SimpleDialogFragm
                 .show(this.getSupportFragmentManager(), SimpleDialogFragment.TAG);
     }
 
+    @OnClick(R.id.simple_dialog_one_button_button)
+    void onOneButtonButtonClicked() {
+        SimpleDialogFragment.newInstance(this.getString(R.string.dialog_title), this.getString(R.string.dialog_message), this.getString(R.string.dialog_button_positive), SIMPLE_DIALOG_ONE_BUTTON_REQUEST_CODE)
+                .show(this.getSupportFragmentManager(), SimpleDialogFragment.TAG);
+    }
+
+    @OnClick(R.id.simple_dialog_two_buttons_button)
+    void onTwoButtonsButtonClicked() {
+        SimpleDialogFragment.newInstance(this.getString(R.string.dialog_title), this.getString(R.string.dialog_message), this.getString(R.string.dialog_button_positive), this.getString(R.string.dialog_button_negative), SIMPLE_DIALOG_TWO_BUTTONS_REQUEST_CODE)
+                .show(this.getSupportFragmentManager(), SimpleDialogFragment.TAG);
+    }
+
+    @OnClick(R.id.simple_dialog_not_cancelable_button)
+    void onNotCancelableButtonClicked() {
+        SimpleDialogFragment.newInstance(this.getString(R.string.dialog_title), this.getString(R.string.dialog_message), this.getString(R.string.dialog_button_positive), false, SIMPLE_DIALOG_NOT_CANCELABLE_REQUEST_CODE)
+                .show(this.getSupportFragmentManager(), SimpleDialogFragment.TAG);
+    }
+
     @Override
     public void onPositiveButtonClicked(DialogInterface dialog, Integer requestCode) {
-        if (SIMPLE_DIALOG_REQUEST_CODE == requestCode) {
-            Toast.makeText(this, "Ok button clicked", Toast.LENGTH_SHORT).show();
+        if (requestCode != null) {
+            switch (requestCode) {
+                case SIMPLE_DIALOG_ONE_BUTTON_REQUEST_CODE:
+                    Toast.makeText(this, "One button - Positive button clicked", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case SIMPLE_DIALOG_TWO_BUTTONS_REQUEST_CODE:
+                    Toast.makeText(this, "Two buttons - Positive button clicked", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case SIMPLE_DIALOG_NOT_CANCELABLE_REQUEST_CODE:
+                    Toast.makeText(this, "Not cancelable - Positive button clicked", Toast.LENGTH_SHORT).show();
+                    break;
+            }
         }
     }
 
     @Override
     public void onNegativeButtonClicked(DialogInterface dialog, Integer requestCode) {
-        if (SIMPLE_DIALOG_REQUEST_CODE == requestCode) {
-            Toast.makeText(this, "Cancel button clicked", Toast.LENGTH_SHORT).show();
+        if (requestCode != null) {
+            switch (requestCode) {
+                case SIMPLE_DIALOG_ONE_BUTTON_REQUEST_CODE:
+                    // No negative button
+                    break;
+
+                case SIMPLE_DIALOG_TWO_BUTTONS_REQUEST_CODE:
+                    Toast.makeText(this, "Two buttons - Negative button clicked", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case SIMPLE_DIALOG_NOT_CANCELABLE_REQUEST_CODE:
+                    // No negative button
+                    break;
+            }
         }
     }
 }
